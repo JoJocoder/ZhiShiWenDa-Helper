65c1e4fb2ba4a513e2c1e20a17b1876cd3d82be7
Jayden <zhuceregister123@163.com>
2018-01-11 16:34:18 +0800

    Update baiduocr.py

--- baiduocr.py
+++ baiduocr.py
@@ -1,10 +1,9 @@
 from aip import AipOcr
 
 """ 你的 APPID AK SK """
-APP_ID = '10673926'
-API_KEY = 'XVfNGUOZj25Qr4GsMuS6A1ZL'
-SECRET_KEY = 'cTNPEvjmch78gAeLoU9RVirdqLGgblCB '
-path=r'C:\Users\Janton\Desktop\ZhiShiWenDa-Helper-master\image\region3.png'
+APP_ID = ''
+API_KEY = ''
+SECRET_KEY = ''
 client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
 def get_text_from_image(image):
