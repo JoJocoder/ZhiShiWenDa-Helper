df05146efc348ef5412a163e9734b8c4cd9cdfe8
Jayden <zhuceregister123@163.com>
2018-01-11 16:33:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 # -*- coding:utf-8 -*-
-
-app_id = "10661627"
-app_key = "h5xcL0m4TB8fiiFWoysn7uxt"
-app_secret = "HGA1cgXzz80douKQUpII7K77TYWSGcfW"
-
-
+#自行修改路径
+driver_location=r"C:\Users\Janton\Desktop\ZhiShiWenDa-Helper-master\lib\ChromDriver\chromedriver.exe"
+image_directory = "C:\\Users\\Janton\\Desktop\\ZhiShiWenDa-Helper-master\\Images\\"
 # 截取题目
 left = 90 # 距离左边的像素
 top = 250 # 距离顶部的像素
 right = 70 # 距离右边的像素
 bottom = 600 #距离顶部的像素
+#1080P分辨率
